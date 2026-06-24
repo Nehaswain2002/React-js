25b6debdfc32da7e25444f35bc6b7af01001194c
Neha Swain <nehaswain7@gmail.com>
2026-06-24 23:55:20 +0530

    Add multiple folders

---
+++ 02-component/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <p className="nav">home</p>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ 03-props/src/App.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="parent">
+      <div className="card">
+   
+    <img className='imge' src="https://www.shutterstock.com/shutterstock/photos/2404973101/display_1500/stock-photo-panda-bear-bubu-dudu-couple-pink-jacket-cute-and-pretty-2404973101.jpg" alt="" />
+     <h1>Hii , How are you ?</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore, debitis!</p>
+    <button className='button'>View profile</button>
+    </div>
+     <div className="card">
+   
+    <img className='imge' src="https://www.shutterstock.com/shutterstock/photos/2404973101/display_1500/stock-photo-panda-bear-bubu-dudu-couple-pink-jacket-cute-and-pretty-2404973101.jpg" alt="" />
+     <h1>Hii , How are you ?</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore, debitis!</p>
+    <button className='button'>View profile</button>
+    </div>
+     <div className="card">
+   
+    <img className='imge' src="https://www.shutterstock.com/shutterstock/photos/2404973101/display_1500/stock-photo-panda-bear-bubu-dudu-couple-pink-jacket-cute-and-pretty-2404973101.jpg" alt="" />
+     <h1>Hii , How are you ?</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore, debitis!</p>
+    <button className='button'>View profile</button>
+    </div>
+     <div className="card">
+   
+    <img className='imge' src="https://www.shutterstock.com/shutterstock/photos/2404973101/display_1500/stock-photo-panda-bear-bubu-dudu-couple-pink-jacket-cute-and-pretty-2404973101.jpg" alt="" />
+     <h1>Hii , How are you ?</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore, debitis!</p>
+    <button className='button'>View profile</button>
+    </div>
+    </div>
+    
+  )
+}
+
+export default App
